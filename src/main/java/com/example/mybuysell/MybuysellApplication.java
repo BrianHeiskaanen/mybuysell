3c673583c5b3ca1b08e72b6dd0ab39b90e64d050
Maxim Fomov <maksimfomov26@gmail.com>
2022-04-27 19:00:15 +0300

    init: Created project

---
+++ src/main/java/com/example/mybuysell/MybuysellApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybuysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybuysellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybuysellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mybuysell/MybuysellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybuysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybuysellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

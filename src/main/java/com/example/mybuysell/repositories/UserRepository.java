34bc7c8d3e1e1417ab91c910b07043494bab4482
Maxim Fomov <maksimfomov26@gmail.com>
2022-04-28 16:11:22 +0300

    feat: Added spring security

---
+++ src/main/java/com/example/mybuysell/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.mybuysell.repositories;
+
+import com.example.mybuysell.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
---
+++ src/main/java/com/example/mybuysell/services/UserService.java
@@ -0,0 +1,27 @@
+package com.example.mybuysell.services;
+
+import com.example.mybuysell.models.User;
+import com.example.mybuysell.models.enums.Role;
+import com.example.mybuysell.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean createUser(User user) {
+        if(userRepository.findByEmail(user.getEmail()) != null) return false;
+        user.setActive(true);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+        userRepository.save(user);
+        log.info("Saving new User with email: {}", user.getEmail());
+        return true;
+    }
+}
